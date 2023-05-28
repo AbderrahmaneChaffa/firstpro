4fc7f2cea121bacb63b203b06bc26ffff4839a83
Chaffa Abderrahmane <ab.chaffa31@gmail.com>
2023-05-28 18:19:02 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>HTS Project</title>
+        
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+        <h1>Salam <?php echo $nom." ".$prenom?> </h1>
+        
+        <!--h2>Les cours </!--h2>
+            <--ul>
+                <?php //foreach($cours as $values): ?>
+                <li><--?php echo $values;?></li>
+                <?php //endforeach;?>
+            </ul-->
+
+        <script src="" async defer></script>
+    </body>
+</html>
+
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+GET     :Lecture
+POST    :Ajouter
+PUT     :Modification Complete
+PATCH   :Modification partielle
+DELETE  :Supprimer
+
+CONNECT
+OBTIONS
+
+*/
+
+Route::get('/{nom}/{prenom}', function (Illuminate\Http\Request $request) {
+    //dd($request);
+    return view('welcome',[
+        'nom'=> $request->nom,
+        'prenom'=> $request->prenom
+        /*'prenom' => "abderrahmane",
+        'cours' =>  ['PHP', 'HTML','LARAVEL', 'CSS']*/
+    ]);
+});
